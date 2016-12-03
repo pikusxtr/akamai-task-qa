52bcb7a767966f9736c877e15595dcaf6b01ac63
Krzysztof Piekos <pikusxtr@gmail.com>
2016-12-03 09:10:44 +0100

    pom update, methods and tests update

--- src/main/java/com/pages/MainPage.java
+++ src/main/java/com/pages/MainPage.java
@@ -14,96 +14,98 @@ import java.util.List;
 
 public class MainPage extends AbstractPage {
 
-  private static final int TIMEOUT = 7;
-  List<WebElement> searchResultsList;
+    private static final int TIMEOUT = 7;
+    private List<WebElement> searchResultsList;
 
-  @FindBy(id = "keyword")
-  private WebElement searchByKeywordText;
+    @FindBy(id = "keyword")
+    private WebElement searchByKeywordText;
 
-  @FindBy(id = "loc_placeholder")
-  private WebElement locationsField;
+    @FindBy(id = "loc_placeholder")
+    private WebElement locationsField;
 
-  @FindBy(id = "jSearchSubmit")
-  private WebElement searchBtn;
+    @FindBy(id = "jSearchSubmit")
+    private WebElement searchBtn;
 
-  private By locationTextInput = By.cssSelector("div#location_facet_chzn input.default");
-  private By searchFinished = By.cssSelector("div#jPaginationHldr");
-  private By searchResultsItemsLocator =
-      By.cssSelector("div#jobs_filters_area > div#job_results_list_hldr > div[id^='job_list_']");
+    private By locationTextInput = By.cssSelector("div#location_facet_chzn input.default");
+    private By searchFinished = By.cssSelector("div#jPaginationHldr");
+    private By searchResultsItemsLocator =
+            By.cssSelector("div#jobs_filters_area > div#job_results_list_hldr > div[id^='job_list_']");
 
-  private By pageVisible =
-      By.cssSelector("div#job_results_list_hldr, div#job_no_results_list_hldr");
+    private By pageVisible =
+            By.cssSelector("div#job_results_list_hldr, div#job_no_results_list_hldr");
 
-  private String url;
+    private String url;
 
-  public MainPage(WebDriver driver, String url) {
-    super(driver);
-    this.url = url;
-    PageFactory.initElements(driver, this);
-    open();
-    // Check that we're on the right page.
-    if (!(driver.getCurrentUrl().contains("referrals"))) {
-      throw new IllegalStateException(
-          "This is not the " + this.getClass().getSimpleName() + " page");
+    public MainPage(WebDriver driver, String url) {
+        super(driver);
+        this.url = url;
+        PageFactory.initElements(driver, this);
+        open();
+
     }
-    WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
-    wait.until(ExpectedConditions.visibilityOfElementLocated(pageVisible));
-  }
 
-  public MainPage open() {
-    driver.get(url);
-    return this;
-  }
+    public MainPage open() {
+        driver.get(url);
+        // Check that we're on the right page.
+        if (!(driver.getCurrentUrl().contains("referrals"))) {
+            throw new IllegalStateException(
+                    "This is not the " + this.getClass().getSimpleName() + " page");
+        }
+        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(pageVisible));
+        return this;
+    }
 
-  public MainPage search() {
-    searchBtn.click();
+    public MainPage search() {
+        searchBtn.click();
 
-    WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
-    wait.until(ExpectedConditions.presenceOfElementLocated(searchFinished));
-    return this;
-  }
+        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
+        wait.until(ExpectedConditions.presenceOfElementLocated(searchFinished));
+        return this;
+    }
 
-  public MainPage setJobKeyword(String keyword) {
-    setElementText(searchByKeywordText, keyword);
-    return this;
-  }
+    public MainPage setJobKeyword(String keyword) {
+        setElementText(searchByKeywordText, keyword);
+        return this;
+    }
 
-  public MainPage enterLocation(String location) {
-    locationsField.click();
-    WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
+    public MainPage enterLocation(String location) {
+        locationsField.click();
+        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
 
-    WebElement locationTextInputElem =
-        wait.until(ExpectedConditions.presenceOfElementLocated(locationTextInput));
-    setElementText(locationTextInputElem, location);
-    return this;
-  }
+        WebElement locationTextInputElem =
+                wait.until(ExpectedConditions.presenceOfElementLocated(locationTextInput));
+        setElementText(locationTextInputElem, location);
+        return this;
+    }
 
-  public MainPage selectLocationFromResults(Locations location) {
-    WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
-    By textToGetFromLocation = By.cssSelector("ul.chzn-results > li.active-result");
-    List<WebElement> locationResultsContainerActiveElem =
-        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(textToGetFromLocation));
-    locationResultsContainerActiveElem.forEach(
-        webElement -> {
-          if (webElement.getText().contentEquals(location.getFullLocationName()))
-            webElement.click();
-        });
-    return this;
-  }
+    //refactor method, extract wait
+    public MainPage selectLocationFromResults(Locations location) {
+        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
+        By textToGetFromLocation = By.cssSelector("ul.chzn-results > li.active-result");
+        List<WebElement> locationResultsContainerActiveElem =
+                wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(textToGetFromLocation));
+        locationResultsContainerActiveElem.forEach(
+                webElement -> {
+                    if (webElement.getText().contentEquals(location.getFullLocationName()))
+                        webElement.click();
+                });
+        return this;
+    }
 
-  public MainPage assertAnyResultsReturned() {
-    searchResultsList = driver.findElements(searchResultsItemsLocator);
-    System.out.println("Search results jobs count=" + searchResultsList.size());
-    Assert.assertTrue(!searchResultsList.isEmpty(), "Search results list is not empty");
-    return this;
-  }
+    public MainPage assertAnyResultsReturned() {
+        searchResultsList = driver.findElements(searchResultsItemsLocator);
+        System.out.println("Search results jobs count=" + searchResultsList.size());
+        Assert.assertTrue(!searchResultsList.isEmpty(), "Search results list is not empty");
+        return this;
+    }
 
-  public MainPage assertNumberOfResultsReturned(int resultsCount) {
-    searchResultsList = driver.findElements(searchResultsItemsLocator);
-    Assert.assertEquals(
-        searchResultsList.size(),
-        resultsCount,
-        "Number of search results is equal to " + resultsCount);
-    return this;
-  }
+    public MainPage assertNumberOfResultsReturned(int resultsCount) {
+        searchResultsList = driver.findElements(searchResultsItemsLocator);
+        Assert.assertEquals(
+                searchResultsList.size(),
+                resultsCount,
+                "Number of search results is equal to " + resultsCount);
+        return this;
+    }
 }
--- src/test/java/com/tests/AkamaiTest.java
+++ src/test/java/com/tests/AkamaiTest.java
@@ -4,33 +4,39 @@ import com.data.AppUrls;
 import com.data.Locations;
 import com.pages.MainPage;
 import com.pages.WebDriverTestBase;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class AkamaiTest extends WebDriverTestBase {
-  private MainPage mainPage;
+    private MainPage mainPage;
 
-  @BeforeMethod
-  public void openMainPage() {
-    mainPage = new MainPage(driver, AppUrls.PROD.getUrl());
-  }
+    @BeforeClass
+    public void openMainPage() {
+        mainPage = new MainPage(driver, AppUrls.PROD.getUrl());
+    }
 
-  @Test
-  public void anyResultsShouldBeReturnedForKeywordLocation() {
+    @Test
+    public void anyResultsShouldBeReturnedForKeywordLocation() {
 
-    mainPage
-        .setJobKeyword("Test")
-        .enterLocation("Krakow")
-        .selectLocationFromResults(Locations.PL_KRK)
-        .search()
-        .assertAnyResultsReturned();
-  }
+        mainPage
+                .setJobKeyword("Test")
+                .enterLocation("Krakow")
+                .selectLocationFromResults(Locations.PL_KRK)
+                .search()
+                .assertAnyResultsReturned();
+    }
 
-  @Test
-  public void noResultsShouldBeReturnedForKeywordWithEmptyLocation() {
-    mainPage
-        .setJobKeyword("XXX")
-        .search()
-        .assertNumberOfResultsReturned(0);
-  }
+    @Test
+    public void noResultsShouldBeReturnedForKeywordWithEmptyLocation() {
+        mainPage
+                .setJobKeyword("XXX")
+                .search()
+                .assertNumberOfResultsReturned(0);
+    }
+
+    @AfterMethod
+    public void resetPage() {
+        mainPage.open();
+    }
 }
